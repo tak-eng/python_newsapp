6ee0d1f393b836ff757292f6cc449fb635462646
tak-eng <shibamoto2411@gmail.com>
2021-01-12 21:54:47 +0900

    ビューファイルを修正

--- newsproject/settings.py
+++ newsproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '8vgf^y!deyunyckgg2eh08x^s5sb)fyfcfm4skp=$!hcf5$i5u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
